11f4f42f906230b50077708a10f3a9961bc56184
Sotatek-ThooLuu <thooluu@sotatek.com>
2022-07-25 14:37:21 +0700

    first commit

---
+++ src/caverHelper.js
@@ -0,0 +1,12 @@
+const Caver = require('caver-js');
+
+const getCaverVersion = async () => {
+  const caver = new Caver('https://public-node-api.klaytnapi.com/v1/baobab');
+  const caverVersion = await caver.rpc.klay.getClientVersion();
+  console.log(`---caver version:`, caverVersion);
+  return caverVersion;
+};
+getCaverVersion();
+module.exports = {
+  getCaverVersion,
+};
---
+++ src/index.js
@@ -0,0 +1,50 @@
+// const Caver = require('caver-js');
+const { getCaverVersion } = require('./caverHelper');
+
+module.exports.onRpcRequest = async ({ origin, request }) => {
+  switch (request.method) {
+    case 'hello': {
+      const fees = JSON.parse(await getFees());
+      const baseFee = parseFloat(fees.currentBaseFee);
+      const safeLow = Math.ceil(baseFee + parseFloat(fees.safeLow));
+      const standard = Math.ceil(baseFee + parseFloat(fees.standard));
+      const fastest = Math.ceil(baseFee + parseFloat(fees.fastest));
+
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: `Gas Fees`,
+            description: 'Current Gas Fees from etherchain.org:',
+            textAreaContent: `
+              Low: ${safeLow}\n
+              Average: ${standard}\n
+              High: ${fastest}\n`,
+            // Caver version: ${caverVersion}
+          },
+        ],
+      });
+      // return wallet.request({
+      //   method: 'snap_confirm',
+      //   params: [
+      //     {
+      //       prompt: `Hello, ${origin}!`,
+      //       description:
+      //         'This custom confirmation is just for display purposes.',
+      //       textAreaContent:
+      //         'But you can edit the snap source code to make it do something, if you want to!',
+      //     },
+      //   ],
+      // });
+    }
+    default:
+      throw new Error('Method not found.');
+  }
+};
+
+async function getFees() {
+  const response = await fetch('https://www.etherchain.org/api/gasPriceOracle');
+  const caverVersion = await getCaverVersion();
+  console.log(caverVersion);
+  return response.text();
+}
