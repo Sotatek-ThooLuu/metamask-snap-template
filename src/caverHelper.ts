89999fcb4f0bfb21f1d557e9cedcf57524c6576a
Sotatek-ThooLuu <thoo.luu@sotatek.com>
2022-08-08 14:39:12 +0700

    feat: send txn

--- src/caverHelper.ts
+++ src/caverHelper.ts
@@ -1,5 +1,32 @@
-import Caver from "caver-js";
+import Caver, { SingleKeyring, ValueTransfer } from "caver-js";
+
+// let keyring: SingleKeyring;
+// export const initKeyring = (address: string, key: string) => {
+
+// }
 
 export const caver = new Caver("https://public-node-api.klaytnapi.com/v1/baobab");
 
-export const sign = async (message: string, address: string) => await caver.klay.sign(message, address);
+export const send = async (_from: string, _to: string, _key: string, _value: number) => {
+    const keyring: SingleKeyring = caver.wallet.keyring.create(_from, _key);
+    caver.wallet.add(keyring);
+
+    const value = caver.utils.toHex(caver.utils.convertToPeb(_value, "KLAY"));
+    const gas = caver.utils.toHex(caver.utils.convertToPeb(30000, "peb"));
+
+    const valueTransfer: ValueTransfer = caver.transaction.valueTransfer.create({
+        from: _from,
+        to: _to,
+        value, gas
+    });
+    await valueTransfer.fillTransaction();
+    await caver.wallet.sign(keyring.address, valueTransfer);
+    
+    const rlpEncoded = valueTransfer.getRLPEncoding();
+    const receipt = await caver.rpc.klay.sendRawTransaction(rlpEncoded);
+    
+    console.log("26", receipt);
+    
+
+    return receipt;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,15 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
-import { sign } from './caverHelper';
+import { send } from './caverHelper';
 
-const addr: string = "0xABB70823C20c977302C89197E6A87AC09D184d72";
-// const privateKey: string = "cfe490ba17d46c5aa7c995249c05ceac71e5bd3c0ca8c0c880b4b7f8ad4fe092";
-const data: string = "data to sign";
+const from: string = '0x4cE51d78E0Afe5baccb64C30Bd0A8651bF36e8BB';      // acc1
+const to: string = '0xc7ec3eC37437eb4474F19B4f2F2f52d577Ab6ec5';        // acc2
+const key: string = '0xc5309be41d382917f9fb5f1f075cd062409acf7c6b97357b5db9bca82ae18331';
 
 export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
   switch (request.method) {
-    case 'sign':
-      const res = wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
-
-      if (res) {
-        try {
-          const resSign = await sign(data, addr);
-          console.log("Res 27", resSign);
-          
-        } catch(err) { 
-          console.error("Err 30", err.message);
-        };
-      }
-      
-      return res;
+    case 'send':      
+      const sendValue = send(from, to, key, 1);
+      return sendValue;
     default:
       throw new Error('Method not found.');
   }
