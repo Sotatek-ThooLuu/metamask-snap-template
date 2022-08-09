8941f1d94364c6aedad4a68008fd3b5bdbc1c495
Sotatek-ThooLuu <thoo.luu@sotatek.com>
2022-08-09 09:41:14 +0700

    feat: confirm txn popup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
 import { send } from './caverHelper';
 
-const from: string = '0x4cE51d78E0Afe5baccb64C30Bd0A8651bF36e8BB';      // acc1
-const to: string = '0xc7ec3eC37437eb4474F19B4f2F2f52d577Ab6ec5';        // acc2
-const key: string = '0xc5309be41d382917f9fb5f1f075cd062409acf7c6b97357b5db9bca82ae18331';
+const from: string = '0x4cE51d78E0Afe5baccb64C30Bd0A8651bF36e8BB';                        // acc1
+const key: string = '0xc5309be41d382917f9fb5f1f075cd062409acf7c6b97357b5db9bca82ae18331'; // klaytn private key
+// const to: string = '0xc7ec3eC37437eb4474F19B4f2F2f52d577Ab6ec5';                          // acc2
 
 export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
   switch (request.method) {
-    case 'send':      
-      const sendValue = send(from, to, key, 1);
-      return sendValue;
+    case 'send':
+      const to = request['to'];
+      const value = request['value'];
+
+      const confirm = await wallet.request({
+        method: 'snap_confirm',
+        params: [{
+          prompt: `Hello, ${origin}`,
+          description: 'Please confirm transaction',
+          textAreaContent: `To: ${to}\nValue: ${value} KLAY`
+        }]
+      });
+
+      if (confirm) {
+        const sendValue = send(from, to, key, value);
+        return sendValue;
+      }
+
+      return confirm;
     default:
       throw new Error('Method not found.');
   }
